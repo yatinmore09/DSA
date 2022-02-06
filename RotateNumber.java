ac09429f1fadedffb6d36c7b64ba94bd522ad6e8
yatinmore09 <82366777+yatinmore09@users.noreply.github.com>
2022-02-06 18:40:06 +0530

    rotate number

---
+++ RotateNumber.java
@@ -0,0 +1,61 @@
+package getttingStarted;
+
+/*
+1. You are given two numbers n and k. You are required to rotate n, k times to the right. If k is positive, rotate to the right i.e. remove rightmost digit and make it leftmost. Do the reverse for negative value of k. Also k can have an absolute value larger than number of digits in n.
+2. Take as input n and k.
+3. Print the rotated number.
+4. Note - Assume that the number of rotations will not cause leading 0's in the result. e.g. such an input will not be given
+   n = 12340056
+   k = 3
+   r = 05612340
+Input Format
+	"n" where n is any integer.
+	"K" where k is any integer.
+Output Format
+	"r", the rotated number
+ */
+import java.util.Scanner;
+
+public class RotateNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int k = scn.nextInt();
+
+		int nod = 0;
+
+		int temp = n;
+		while (temp > 0) {
+			temp = temp / 10;
+			nod++;
+		}
+
+		k = k % nod;
+		
+		if (k < 0) {
+			k = k + nod;
+		}
+		
+		int div = 1;
+		int mult = 1;
+
+		for (int i = 1; i <= nod; i++) {
+			if (i <= k) {
+				div = div * 10;
+			} else {
+				mult = mult * 10;
+			}
+		}
+
+		int q = n / div;
+		int r = n % div;
+
+		int rot = r * mult + q;
+
+		System.out.println(rot);
+	}
+
+}
